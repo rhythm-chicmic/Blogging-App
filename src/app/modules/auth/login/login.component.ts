40deb1a8bfee6c9afb2fd7e57c09c83d6413684c
Rhythm-08 <rhythmsharma808@gmail.com>
2023-03-23 23:20:59 +0530

    validations added

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormGroupDirective, Validators } from '@angular/forms';
+import { REGEX } from 'src/app/common/constants';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  LoginForm!:FormGroup
+  @ViewChild(FormGroupDirective)
+  formDirective!:FormGroupDirective;
+  submitted = false;
+  constructor(private fb:FormBuilder){
 
+    this.initLoginForm();
+  }
+  initLoginForm(){
+  this.LoginForm = this.fb.group({
+    email: new FormControl('', [Validators.required, Validators.email,Validators.pattern(REGEX.EMAIL)]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6) ,Validators.pattern(REGEX.PASSWORD)])
+  })
+}
+get controls(){
+  return this.LoginForm.controls;
+}
+login(){
+   if((this.LoginForm as FormGroup).valid){
+    console.log(this.LoginForm.value);
+    this.formDirective.resetForm();
+    }
+    else{
+      this.submitted =false;
+      this.formDirective.resetForm();
+
+    }
+}
 }
--- src/app/modules/auth/register/register.component.ts
+++ src/app/modules/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -8,4 +9,6 @@ import { Component } from '@angular/core';
 export class RegisterComponent {
   hide = true;
   startDate = new Date(1990, 0, 1);
+  RegisterForm!:FormGroup
+  constructor(private fb:FormBuilder){}
 }
