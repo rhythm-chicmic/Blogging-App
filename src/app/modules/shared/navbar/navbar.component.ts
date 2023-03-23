88045d5fae6e1ba2c88523307b1c02ab0bfdead0
Rhythm-08 <rhythmsharma808@gmail.com>
2023-03-23 22:57:19 +0530

    added routes to navbar

--- src/app/modules/shared/navbar/navbar.component.ts
+++ src/app/modules/shared/navbar/navbar.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { PATHS } from 'src/app/common/constants';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
+  constructor(private route:Router){}
+  signIn(){
+    this.route.navigate([PATHS.AUTH.LOGIN]);
+  }
+  signUp(){
+    this.route.navigate([PATHS.AUTH.REGISTER]);
+  }
 
 }
