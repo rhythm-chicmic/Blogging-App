e9e2dd1444630e8d792648148cc29adb0ce6ae98
Rhythm-08 <rhythmsharma808@gmail.com>
2023-03-26 15:27:53 +0530

    editor Mode completed

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WriteBlogsComponent } from './write-blogs/write-blogs.component';
-import { NgxEditorModule } from 'ngx-editor';
-import { FormsModule } from '@angular/forms';
+import { NgxEditorModule,schema } from 'ngx-editor';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { PATHS } from 'src/app/common/constants';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -19,7 +19,42 @@ const routes:Routes=[
   ],
   imports: [
     CommonModule,
-    NgxEditorModule,
+    ReactiveFormsModule,
+    NgxEditorModule.forRoot({
+      locals: {
+        // menu
+        bold: 'Bold',
+        italic: 'Italic',
+        code: 'Code',
+        blockquote: 'Blockquote',
+        underline: 'Underline',
+        strike: 'Strike',
+        bullet_list: 'Bullet List',
+        ordered_list: 'Ordered List',
+        heading: 'Heading',
+        h1: 'Header 1',
+        h2: 'Header 2',
+        h3: 'Header 3',
+        h4: 'Header 4',
+        h5: 'Header 5',
+        h6: 'Header 6',
+        align_left: 'Left Align',
+        align_center: 'Center Align',
+        align_right: 'Right Align',
+        align_justify: 'Justify',
+        text_color: 'Text Color',
+        background_color: 'Background Color',
+
+        // popups, forms, others...
+        url: 'URL',
+        text: 'Text',
+        openInNewTab: 'Open in new tab',
+        insert: 'Insert',
+        altText: 'Alt Text',
+        title: 'Title',
+        remove: 'Remove',
+      },
+    }),
     FormsModule,
     RouterModule.forChild(routes),
     SharedModule
--- src/app/modules/main/write-blogs/write-blogs.component.ts
+++ src/app/modules/main/write-blogs/write-blogs.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Editor } from 'ngx-editor';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Editor,Toolbar,Validators } from 'ngx-editor';
 @Component({
   selector: 'app-write-blogs',
   templateUrl: './write-blogs.component.html',
@@ -7,7 +8,30 @@ import { Editor } from 'ngx-editor';
 })
 export class WriteBlogsComponent implements OnInit, OnDestroy{
   editor!: Editor;
+  writeForm!:FormGroup
   html!: '';
+  constructor(private fb:FormBuilder){
+    this.initWriteForm();
+  }
+  toolbar: Toolbar = [
+    ['bold', 'italic'],
+    ['underline', 'strike'],
+    ['code', 'blockquote'],
+    ['ordered_list', 'bullet_list'],
+    [{ heading: ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] }],
+    ['link', 'image'],
+    ['text_color', 'background_color'],
+    ['align_left', 'align_center', 'align_right', 'align_justify'],
+  ];
+  initWriteForm(){
+    this.writeForm =this.fb.group({
+      editorContent: new FormControl('', Validators.required()),
+    });
+  }
+
+  OnSubmit(){
+    console.log(this.writeForm.value);
+  }
 
   ngOnInit(): void {
     this.editor = new Editor();
