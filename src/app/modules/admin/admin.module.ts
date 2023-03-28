c294140e3b694523e24c3ca079151b8530eeedfb
Rhythm08 <rhythm.sharma@chicmic.co.in>
2023-03-28 09:46:10 +0530

    minor commit

---
+++ src/app/modules/admin/admin.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminProfileComponent } from './admin-profile/admin-profile.component';
+
+
+
+@NgModule({
+  declarations: [
+    AdminProfileComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class AdminModule { }
